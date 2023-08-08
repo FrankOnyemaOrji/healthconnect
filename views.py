7a09398e3aeaccb656167cc2712df638a66744b8
Frank Onyema Orji <f.orji@alustudent.com>
2023-08-08 15:29:26 +0100

    Resources function done

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
+import requests
 from flask_login import login_required
 from . import db
-
 
 views = Blueprint('views', __name__)
 
@@ -9,3 +9,14 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def index():  # put application's code here
     return render_template('index.html')
+
+
+@views.route('/resource')
+def resource():
+    # Get the health topics from the MyHealthfinder API
+    response = requests.get("https://api.myhealthfinder.gov/v2/topics")
+    topics = response.json()
+
+    # Render the HTML template with the health topics
+    return render_template("resources.html", topics=topics)
+
